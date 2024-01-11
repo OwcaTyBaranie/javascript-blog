4cec208f25a496f35a9a12cf91bb33ee14ee0147
Piotr Wawrzyniak <hola.wawrzyniak@gmail.com>
2024-01-11 12:20:44 +0100

    Add all rest instuctions to js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 function titleClickHandler(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
     console.log(event);
@@ -21,10 +22,16 @@ for(let activeLink of activeLinks){
   }
 
   /* get 'href' attribute from the clicked link */
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector:', articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
+const targetArticle = document.querySelector(articleSelector);
+console.log('targetArticle:', targetArticle);
 
   /* add class 'active' to the correct article */
+targetArticle.classList.add('active');
+console.log('targetArticle:',targetArticle);
   }
 
   const links = document.querySelectorAll('.titles a');
